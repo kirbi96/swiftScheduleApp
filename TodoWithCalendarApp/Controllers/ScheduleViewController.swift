ab5105ddaf0a60e3e42ab9e201bd2ca5cf8cf426
kirill <repetitorkazan@mail.ru>
2022-02-04 15:22:20 +0300

    cell model

--- TodoWithCalendarApp/Controllers/ScheduleViewController.swift
+++ TodoWithCalendarApp/Controllers/ScheduleViewController.swift
@@ -70,11 +70,12 @@ class ScheduleViewController: UIViewController {
 
 extension ScheduleViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 24
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: idScheduleCell, for: indexPath) as! ScheduleTableViewCell
+        cell.setContent(index: indexPath.row)
         return cell
     }
     
@@ -104,7 +105,6 @@ extension ScheduleViewController {
         
         calendarHeigthConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
         calendar.addConstraint(calendarHeigthConstraint)
-        
         NSLayoutConstraint.activate([
             calendar.topAnchor.constraint(equalTo: view.topAnchor, constant:  90),
             calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
@@ -112,12 +112,19 @@ extension ScheduleViewController {
         ])
         
         view.addSubview(showHideButton)
-        
         NSLayoutConstraint.activate([
             showHideButton.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant:  0),
             showHideButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
             showHideButton.widthAnchor.constraint(equalToConstant: 100),
             showHideButton.heightAnchor.constraint(equalToConstant: 20),
+        ])
+        
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: showHideButton.bottomAnchor, constant:  10),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
         ])
     }
 }
--- TodoWithCalendarApp/Model/Cell/ScheduleTableViewCell.swift
+++ TodoWithCalendarApp/Model/Cell/ScheduleTableViewCell.swift
@@ -9,12 +9,58 @@ import UIKit
 
 class ScheduleTableViewCell: UITableViewCell {
     
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "TASK"
+        label.textColor = .black
+        label.textAlignment = .right
+        label.adjustsFontSizeToFitWidth = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00:00"
+        label.textColor = .black
+        label.textAlignment = .left
+        label.adjustsFontSizeToFitWidth = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setConstrains()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setContent(index: Int){
+        timeLabel.text = String("\(index):00")
+    }
+    
+    func setConstrains(){
+        self.addSubview(descriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+            descriptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
+            descriptionLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5),
+            descriptionLabel.heightAnchor.constraint(equalToConstant: 25),
+        ])
+        
+        self.addSubview(timeLabel)
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+            descriptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
+            descriptionLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5),
+            descriptionLabel.heightAnchor.constraint(equalToConstant: 25),
+        ])
     }
 }
